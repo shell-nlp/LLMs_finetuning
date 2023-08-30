adda5e3ea836e838279d30ce84f3eb01ce0193f6
刘宇 <n0378@qq.com>
2023-08-30 20:30:19 +0800

    修改为通用的数据加载器

--- dataset.py
+++ dataset.py
@@ -1,51 +1,37 @@
+import re
 from datasets import load_dataset
 from transformers import AutoTokenizer
 
 
-def belle_open_source_500k(data_file, tokenizer, max_len):
-    # https://huggingface.co/datasets/BelleGroup/train_0.5M_CN/blob/main/Belle_open_source_0.5M.json
-    def tokenize(prompt, add_eos_token=True):
-        result = tokenizer(
-            prompt,
-            truncation=True,
-            max_length=max_len,
-            padding=False,
-            return_tensors=None,
-        )
-        if (
-                result["input_ids"][-1] != tokenizer.eos_token_id
-                and len(result["input_ids"]) < max_len
-                and add_eos_token
-        ):
-            result["input_ids"].append(tokenizer.eos_token_id)
-            result["attention_mask"].append(1)
-
-        if add_eos_token and len(result["input_ids"]) >= max_len:
-            result["input_ids"][max_len - 1] = tokenizer.eos_token_id
-            result["attention_mask"][max_len - 1] = 1
-
-        result["labels"] = result["input_ids"].copy()
-        return result
-
-
+def get_dataset(data_file, tokenizer):
+   
     def generate_and_tokenize_prompt(data_point):
+        model_inputs = {}
         instruction = data_point['instruction']
         input_text = data_point["input"]
-        input_text = "Human: " + instruction + input_text + "\n\nAssistant: "
-        input_text = tokenizer.bos_token + input_text if tokenizer.bos_token != None else input_text
-        target_text = data_point["output"] + tokenizer.eos_token
-        full_prompt = input_text + target_text
-        tokenized_full_prompt = tokenize(full_prompt)
-        return tokenized_full_prompt
+        target_text = data_point['output']
+
+        prompt = instruction +"\n"+ input_text
+        prompt_ids = tokenizer.encode(prompt,add_special_tokens=True)
+
+        target_text = tokenizer.encode(target_text,add_special_tokens=False)
+        prompt_len = len(prompt_ids)
+        input_ids = prompt_ids + target_text + [tokenizer.eos_token_id]
+        labels = [tokenizer.pad_token_id] * prompt_len+target_text + [tokenizer.eos_token_id]
+        attention_mask = [1] * len(labels)
+        assert len(input_ids) == len(labels) and len(input_ids)==len(attention_mask),"input_ids 和 labels 长度不一致！"
+        model_inputs["input_ids"]=input_ids
+        model_inputs["labels"]=labels
+        model_inputs["attention_mask"]=attention_mask
+        return model_inputs
 
     data = load_dataset("json", data_files=data_file)["train"]
-    data = data.map(generate_and_tokenize_prompt, num_proc=8)
+    data = data.map(generate_and_tokenize_prompt, num_proc=8,remove_columns=list(data.features))
     return data
 
 
 if __name__ == "__main__":
     check_point = r"E:\models\THUDM\chatglm2-6b-int4"
     tokenizer = AutoTokenizer.from_pretrained(check_point, trust_remote_code=True)
-    ds = belle_open_source_500k("./data/Belle_open_source_0.5M.json", tokenizer, 512)
+    ds = get_dataset("./data/example_data.json", tokenizer)
     print(ds[1])
-    print(tokenizer.decode(ds[1]['input_ids']))
